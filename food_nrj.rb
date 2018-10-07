a96568c886f462bdab99c2180c25b546644ba9dc
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-07 20:30:57 +0200

    A approfondir pour créa d'une methode 'price'

--- food_nrj.rb
+++ food_nrj.rb
@@ -1,19 +1,14 @@
-#une table hash par plat, contenant les indices nrjetiques des aliments
+=begin
+Le but est de créer un hash qui contiendra une base de donnée de repas, 
+avec les apports nutritifs associés (glucides, lipides et protéines).
+
+puts "Indice calorique du plat 'Welsh' :"
 WELSH = Hash.new
 WELSH["plat"] =  "Welsh"
 WELSH["Lipides"] = 38.4
 WELSH["Glucides"] = 18.3
 WELSH["Protéines"] = 36.3
-=begin
-CALORIES = Hash.new
-CALORIES["Lipides"] = 9
-CALORIES["Glucides"] = 4
-CALORIES["Protéines"] = 4
 
-CALORIES.each do |key, value|
-    puts "un #{key} correspond à #{value} calories"
-end
-=end
 def weight_watchers(meal)
     cal = Hash.new
     cal[:lipides] = 9
@@ -27,55 +22,70 @@ def weight_watchers(meal)
     puts "Une portion de WELSH = #{sum} calories"
 end
 weight_watchers(WELSH)
-#declaration d'un hash menu qui contient comme éléments d'autre hash "plat"
-menu = Hash.new
-menu["welsh"] = welsh = {
-    "lipides" => 9,
-    "glucides" => 4,
-    "protéines" => 4
-} 
-menu["frites"] = frites = {
-    "lipides" => 15,
-    "glucides" => 41,
-    "proteines" => 3.4
-}
-menu["biere"] = biere = {
-    "lipides" => 0,
-    "glucides" => 25,
-    "proteines" => 4
-}
-puts frites
-# fonction qui calcul le nombre de calorie a partir de tout les plats qu'on lui rentre en paramettre
+=end
 
-def  meal_weight_watchers(full_meal)
+#declaration d'un hash 'menu' qui contient comme éléments d'autre hash 'aliment'
+@menu = Hash.new
+    @menu["welsh"] = welsh = {
+        "lipides" => 38.4,
+        "glucides" => 18.3,
+        "protéines" => 36.3,
+        "price" => 15
+    } 
+    @menu["frites"] = frites = {
+        "lipides" => 15,
+        "glucides" => 41,
+        "protéines" => 3.4,
+        "price" => 4
+    }
+    @menu["biere"] = biere = {
+        "lipides" => 0,
+        "glucides" => 25,
+        "protéines" => 4,
+        "price" => 5
+    }
+
+# fonction qui calcul le nombre de calorie a partir de tout les plats qu'on lui rentre en paramettre
+def  weight_watchers(full_meal)
     cal = Hash.new
     cal[:lipides] = 9
     cal[:glucides] = 4
     cal[:protéines] = 4
-    puts full_meal["lipides"]
+    #puts full_meal["lipides"]
 
     lipides = full_meal["lipides"] * cal[:lipides]
     glucides = full_meal["glucides"] * cal[:glucides]
     protéines = full_meal["protéines"] * cal[:protéines]
+    
 
     sum_meal = lipides + glucides + protéines
-
-    puts "votre repas contient = #{sum_meal} calories"
+    puts "un aliment contient = #{sum_meal} calories"
+    return sum_meal
 
 end
-meal_weight_watchers(welsh)
 
-def calcul_menu(double_hash)
-    puts "=======> contenu du menu : "
-    puts double_hash
-    puts "---------=============== ---------"
-    double_hash.each do |meal|
-        puts "MEAL !!!!!!!!!!!!!!!!"
-        puts meal
-        meal.each do
-            puts value   
-        end
+
+def meal_weight_watcher(double_hash)
+    total_cal = 0
+#ajoute la sommes des calories de chaque aliment dans la variable 'total_cal'
+    double_hash.values.each do |meal|
+        total_cal = total_cal + weight_watchers(meal)
     end
-end
-calcul_menu(menu)
 
+    puts "votre repas compte un total de #{total_cal} calories."
+    price = 0
+    double_hash.values.each do |meal|
+        price = price + meal["price"]
+    end
+    puts "Et vous aura couté = #{price } $"
+    
+end
+
+def perform
+puts "-------------------------------------------"
+   puts "     Weight watchers    "
+puts "-------------------------------------------"
+puts "Dans votre menu du JOUR :"
+    meal_weight_watcher(@menu)
+end
+perform
